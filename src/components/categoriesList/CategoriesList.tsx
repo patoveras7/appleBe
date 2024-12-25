5a67439cbdd8b74e1037b59a05a0394da12cf1ce
patoveras7 <patricioverasc@gmail.com>
2024-12-25 15:18:28 -0300

    .

--- src/components/categoriesList/CategoriesList.tsx
+++ src/components/categoriesList/CategoriesList.tsx
@@ -9,8 +9,9 @@ const categories = categoriesToPreLoad;
 
   return ( 
     
-         <div className="w-full h-screen p-[10px_30px_10px_35px] flex items-center justify-around flex-wrap gap-[18px] 2xl:gap-[90px] xl:gap-[70px] bg-slate-700">
-    {
+         //<div className="w-full h-screen p-[10px_30px_10px_35px] flex items-center justify-around flex-wrap gap-[18px] 2xl:gap-[90px] xl:gap-[70px] bg-slate-700"> 
+         <div className="w-full h-sreen p-[10px_30px_10px_35px] flex items-center justify-around flex-wrap gap-[30px] bg-slate-700">
+         {   
         categories &&
         categories?.map((categorie) => {
           return (
